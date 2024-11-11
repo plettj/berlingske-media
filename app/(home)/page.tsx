8d95febfa020102a44618764086e622b3af1e20e
plettj <jlplett@uwaterloo.ca>
2024-11-11 14:11:25 +0100

    feat: Anonymization

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,4 @@
-import HomePage from "@/components/layouts.tsx/HomePage";
+import HomePage from "@/components/layouts/HomePage";
 
 export default async function Home() {
   return <HomePage />;
--- components/layouts/HomePage.tsx
+++ components/layouts/HomePage.tsx
@@ -11,6 +11,7 @@ export default function HomePage() {
 
   useEffect(() => {
     const host = window.location.host;
+    console.log(host);
     setIsEn(!host.startsWith("dk."));
   }, []);
 
@@ -20,32 +21,23 @@ export default function HomePage() {
       <h1 className="mt-8 mb-4 -ml-1 text-6xl leading-tight">
         {isEn ? (
           <>
-            CBH Case Competition 2024 <br />
+            CBS Case Competition 2024 <br />
             Berlingske Media
           </>
         ) : (
           <>
-            CBH Case Konkurrence 2024 <br /> Berlingske Media
+            CBS Case Konkurrence 2024 <br /> Berlingske Media
           </>
         )}
       </h1>
       <p className="mb-8 text-balance text-[18px] opacity-90">
-        <a href="https://plett.dev" className="underline hover:opacity-75">
-          Josiah
-        </a>
-        , Thien, Ben, Srishti
+        Team name: <i>Distributed Media</i>
       </p>
       <Button className="w-56" variant="outline" asChild>
         <Link href="https://www.berlingskemedia.dk/">berlingskemedia.dk</Link>
       </Button>
       <p className="my-8 text-balance text-muted-foreground">
-        {isEn ? COPYRIGHT_STRING_EN : COPYRIGHT_STRING_DK}{" "}
-        <a
-          href="https://plett.dev"
-          className="text-muted-foreground hover:underline"
-        >
-          Josiah Plett
-        </a>
+        {isEn ? COPYRIGHT_STRING_EN : COPYRIGHT_STRING_DK} Distributed Media
       </p>
     </div>
   );
