695a7dea32baf01240ec84f922f69c32198cd88c
plettj <jlplett@uwaterloo.ca>
2024-11-12 02:30:04 +0100

    update: Add our mission statement

--- components/layouts/HomePage.tsx
+++ components/layouts/HomePage.tsx
@@ -19,9 +19,9 @@ export default function HomePage() {
   }, []);
 
   return (
-    <div className="relative flex flex-col mx-12 gap-4 my-auto items-start">
+    <div className="relative flex flex-col mx-4 sm:mx-6 md:mx-12 gap-4 my-auto items-start">
       {/* <LocaleButton className="absolute -right-3 -top-16 text-base" /> */}
-      <h1 className="mt-8 mb-4 -ml-1 text-6xl leading-tight">
+      <h1 className="mt-8 mb-4 -ml-1 text-3xl sm:text-4xl lg:text-6xl leading-tight">
         {isEn ? (
           <>
             CBS Case Competition 2024 <br />
@@ -33,9 +33,27 @@ export default function HomePage() {
           </>
         )}
       </h1>
-      <p className="mb-8 text-balance text-[18px] opacity-90">
+      <p className="mb-8 text-balance text-lg opacity-90">
         Team name: <i>{TEAM_NAME}</i>
       </p>
+      <div className="max-w-[35ch] sm:max-w-[50ch]">
+        <p className="mb-8 text-base opacity-90">
+          Berlingske Media led the market with their innovative push from{" "}
+          <b>paper-first</b> to <b>digital-first media</b>, unlocking infinite
+          opportunity.
+        </p>
+        <p className="mb-8 text-base opacity-90">
+          But times have changed, and digital-first isn&apos;t enough for young
+          people. They want <i>personalized</i> news <i>anywhere</i>,{" "}
+          <i>anytime</i>.
+        </p>
+        <p className="mb-8 text-base opacity-90">
+          It&apos;s time for{" "}
+          <span className="text-white">
+            a <b>mobile-first</b> revolution.
+          </span>
+        </p>
+      </div>
       <Button className="w-56" variant="outline" asChild>
         <Link href="https://www.berlingskemedia.dk/">berlingskemedia.dk</Link>
       </Button>
